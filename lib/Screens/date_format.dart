9b86022ce20c5b370544e2873f6e43bb5db24c68
ugocode <obiohaugo5@gmail.com>
2024-03-27 08:04:53 +0100

    new dateformat page

---
+++ lib/Screens/date_format.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class DateFormat extends StatefulWidget {
+  const DateFormat({super.key});
+
+  @override
+  State<DateFormat> createState() => _DateFormatState();
+}
+
+class _DateFormatState extends State<DateFormat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[100],
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Month View',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: SfCalendar(
+                view: CalendarView.month,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Weekly View',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: SfCalendar(
+                view: CalendarView.week,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Monthly Timeline',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: SfCalendar(
+                view: CalendarView.timelineMonth,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Work Week',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: SfCalendar(
+                view: CalendarView.workWeek,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
